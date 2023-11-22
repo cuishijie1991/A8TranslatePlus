f9cdb62252c01754545f8e0c1976b17001c3f9d2
GitHub Action <action@github.com>
2023-11-22 06:52:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/cuishijie1991/a8translateplus/MyBundle.kt
+++ src/main/kotlin/com/github/cuishijie1991/a8translateplus/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.cuishijie1991.a8translateplus
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/cuishijie1991/a8translateplus/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/cuishijie1991/a8translateplus/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.cuishijie1991.a8translateplus.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
